6cb1c3d10317e786f2a2acc7676467188ec66b53
johnwav <102820657+johnwav@users.noreply.github.com>
2023-05-12 20:51:46 +0100

    error

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "@styles/global.css";
+
+export const metadata = {
+  title: "Promotopia",
+  description: "Discover & Share AI prompts",
+};
+
+const RootLayout = ({children}) => {
+  return (
+    <html Lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
