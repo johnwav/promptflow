15b2b408904f0027d45aaa3e6fb39161f7f903a8
johnwav <102820657+johnwav@users.noreply.github.com>
2023-05-13 11:02:35 +0100

    nav wip

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,13 +1,54 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession } from "next-auth";
 
 const Nav = () => {
+  const isUserLoggedIn = useState(true);
   return (
-    <div>
-      Nav
-    </div>
-  )
-}
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center ">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="promptopia logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+      </Link>
 
-export default Nav
+      {/* desk nav */}
+
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <img
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
