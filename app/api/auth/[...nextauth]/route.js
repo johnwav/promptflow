d10631b396944f53728bfa8fe2ca036bae07bd19
johnwav <102820657+johnwav@users.noreply.github.com>
2023-05-15 01:43:47 +0100

    googe cloud + next auth and mongodb 4 user

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,17 +1,51 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/dajabase";
+import User from "@models/user";
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
 
-  async signIn({ prfile }) {},
+    session.user.id = sessionUser.id.toString();
+  },
+
+  async signIn({ profile }) {
+    try {
+      //serveless -> lambda -> dynamoDB
+      await connectToDB();
+
+      //check if a user already exists
+
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      //if not create a new user
+
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          name: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
